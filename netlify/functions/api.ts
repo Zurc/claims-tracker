5aa2654a7adc927a58d7bc64a903f68b163920f7
Zurc <cruzjurado@gmail.com>
2025-02-20 14:02:18 +0000

    changes netlify toml
    testing if api works

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -8,6 +8,7 @@ import { insertClaimSchema } from "../../shared/schema";
 const api = express();
 
 const router = Router();
+
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
 // Get all claims
@@ -17,25 +18,25 @@ router.get("/api/claims", async (_req, res) => {
 });
 
 // Get single claim
-router.get("/api/claims/:id", async (req, res) => {
-  const claim = await storage.getClaim(parseInt(req.params.id));
-  if (!claim) {
-    return res.status(404).json({ message: "Claim not found" });
-  }
-  res.json(claim);
-});
+// router.get("/api/claims/:id", async (req, res) => {
+//   const claim = await storage.getClaim(parseInt(req.params.id));
+//   if (!claim) {
+//     return res.status(404).json({ message: "Claim not found" });
+//   }
+//   res.json(claim);
+// });
 
 // Create new claim
-router.post("/api/claims", async (req, res) => {
-  const parseResult = insertClaimSchema.safeParse(req.body);
-  if (!parseResult.success) {
-    return res.status(400).json({ message: "Invalid claim data" });
-  }
+// router.post("/api/claims", async (req, res) => {
+//   const parseResult = insertClaimSchema.safeParse(req.body);
+//   if (!parseResult.success) {
+//     return res.status(400).json({ message: "Invalid claim data" });
+//   }
 
-  const claim = await storage.createClaim(parseResult.data);
-  res.status(201).json(claim);
-});
+//   const claim = await storage.createClaim(parseResult.data);
+//   res.status(201).json(claim);
+// });
 
-api.use("/api/*", router);
+api.use("/api/", router);
 
 export const handler = serverless(api);
