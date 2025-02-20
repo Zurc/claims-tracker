ebc4c3ca45e36a71e576885d577028d486f5dee5
Zurc <cruzjurado@gmail.com>
2025-02-20 10:47:37 +0000

    functions api change

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -8,6 +8,6 @@ const api = express();
 const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
-api.use("/api/", router);
+api.use("/api/*", router);
 
 export const handler = serverless(api);
